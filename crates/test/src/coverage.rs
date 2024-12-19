d4cb4a5d94090c18b469796250744612fd347dbd
daxpedda <daxpedda@gmail.com>
2024-12-19 18:25:21 +0100

    Use `LLVM_PROFILE_FILE` for test coverage

--- crates/test/src/coverage.rs
+++ crates/test/src/coverage.rs
@@ -23,3 +23,15 @@ pub fn __wbgtest_cov_dump() -> Option<Vec<u8>> {
 pub fn __wbgtest_cov_dump() -> Option<Vec<u8>> {
     None
 }
+
+#[cfg(feature = "unstable-test-coverage")]
+#[wasm_bindgen]
+pub fn __wbgtest_module_signature() -> Option<u64> {
+    Some(unstable_test_coverage::module_signature())
+}
+
+#[cfg(not(feature = "unstable-test-coverage"))]
+#[wasm_bindgen]
+pub fn __wbgtest_module_signature() -> Option<u64> {
+    None
+}
